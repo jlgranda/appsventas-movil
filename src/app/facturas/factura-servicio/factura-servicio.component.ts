87d21e995fe428cb3c1f68035aa685f43ebb0729
kellypaulinc <kelly.narvaez1@gmail.com>
2022-03-16 12:40:25 -0500

    comprobantes rechazados

--- src/app/facturas/factura-servicio/factura-servicio.component.ts
+++ src/app/facturas/factura-servicio/factura-servicio.component.ts
@@ -116,9 +116,7 @@ export class FacturaServicioComponent implements OnInit {
         });
         this.tieneFacturasRecibidas = this.facturasRecibidas.length > 0; //Para mostrar el buscador si hay en que buscar
 
-        this.facturasInvalidas = await this.getComprobantesParaUsuarioConectado();
-        this.facturasInvalidas.push(this.facturas[0]);
-        this.facturasInvalidas.push(this.facturas[0]);
+        this.facturasInvalidas = await this.getComprobantesRechazadosPorUsuarioConectado();
         this.facturasInvalidas.forEach((element) => {
             if (this.getDifferenceInDays(new Date(element.emissionOn), new Date()) < 16) {
                 element.fechaEmision = moment(element.emissionOn.toString()).fromNow();
@@ -134,6 +132,11 @@ export class FacturaServicioComponent implements OnInit {
     getComprobantesPorUsuarioConectado(): Promise<any> {
         //return this.comprobantesService.getComprobantesPorUsuarioConectado('factura').toPromise();
         return this.comprobantesService.getFacturasEmitidasPorUsuarioConectado().toPromise();
+    }
+
+    getComprobantesRechazadosPorUsuarioConectado(): Promise<any> {
+        //return this.comprobantesService.getComprobantesPorUsuarioConectado('factura').toPromise();
+        return this.comprobantesService.getFacturasEmitidasRechazadasPorUsuarioConectado().toPromise();
     }
 
     getComprobantesParaUsuarioConectado(): Promise<any> {
